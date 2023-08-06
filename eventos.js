0b00baa88db39f33eaf5d1f3d3b73dff32fafc7c
luamarante <luuanaamarante@gmail.com>
2023-08-06 11:17:53 -0300

    Modifique js y agregue el div

--- eventos.js
+++ eventos.js
@@ -1,5 +1,13 @@
 // eventos.js
 function saludar() {
-    alert('Hola!');
+    alert('Hola! Soy el botón.');
   }
-
+  
+  // Agregar manejador de eventos para el div
+  document.addEventListener('DOMContentLoaded', function () {
+    const divSaludar = document.getElementById('divSaludar');
+    divSaludar.addEventListener('click', function () {
+      alert('Hola! Soy el div.');
+    });
+  });
+  
